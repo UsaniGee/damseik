4155f6dd854db415cc00866f90d0d81f91782457
UsaniGee <usani3121@gmail.com>
2025-11-16 17:13:46 +0100

    Vite confiq optimized

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,21 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
+  build: {
+    chunkSizeWarningLimit: 1500, // raise limit to reduce noisy warnings
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-router')) return 'vendor-react-router'
+            if (id.includes('@chakra-ui') || id.includes('@emotion')) return 'vendor-chakra'
+            if (id.includes('framer-motion')) return 'vendor-framer-motion'
+            if (id.includes('swiper')) return 'vendor-swiper'
+            if (id.includes('react') || id.includes('react-dom')) return 'vendor-react'
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
